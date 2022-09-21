de30094ad53d88d5aad84f7d72f1f7eff23feda3
Joel Nicolaj Pedersen <nicolajlpedersen@gmail.com>
2022-09-21 14:47:57 +0200

    Fiddeling with Visual Studio
    The import is fucked because of Vetur..

---
+++ src/components/games/GamesItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <li>Test</li>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ src/components/games/GamesList.vue
@@ -0,0 +1,9 @@
+<template>
+  <ul>
+    <games-item></games-item>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { GamesItem } from '@/components/games/GamesItem';
+</script>
